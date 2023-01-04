b067165fdab075f13bc6689d23d0c94c73213411
Entinque <quentin.kuttler@epita.fr>
2023-01-04 21:36:22 +0100

    feat: basic ast function
    added new_AST and free_AST function

--- src/AST/AST.c
+++ src/AST/AST.c
@@ -3,11 +3,29 @@
 struct AST *new_AST(struct token *value, struct AST *left_child,
         struct AST *right_child)
 {
-    // TODO
-    return NULL;
+    if (value == NULL)
+        return NULL;
+
+    struct AST *my_AST = malloc(sizeof(struct AST));
+    
+    if (value == my_AST)
+        return NULL;
+
+    my_AST->value = value;
+    my_AST->left_child = left_child;
+    my_AST->right_child = right_child;
+
+    return my_AST;
 }
 
 void free_AST(struct AST *tree)
 {
-    // TODO
+    if(tree)
+    {
+        free_AST(tree->left_child);
+        free_AST(tree->right_child);
+
+        free_token(token);
+        free(tree);
+    }
 }
