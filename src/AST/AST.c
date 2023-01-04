178ad9adaf63a74cc9bc24a0876d4367bbf46a76
Entinque <quentin.kuttler@epita.fr>
2023-01-04 22:58:04 +0100

    feat: added 'enum AST_type type' in ast struct
    added 'enum AST_type type' in ast struct

--- src/AST/AST.c
+++ src/AST/AST.c
@@ -3,7 +3,7 @@
 struct AST *new_AST(struct token *value, struct AST *left_child,
         struct AST *right_child)
 {
-    if (value == NULL)
+    if (value == NULL || type == NULL)
         return NULL;
 
     struct AST *my_AST = malloc(sizeof(struct AST));
@@ -12,6 +12,7 @@ struct AST *new_AST(struct token *value, struct AST *left_child,
         return NULL;
 
     my_AST->value = value;
+    my_AST->type = type;
     my_AST->left_child = left_child;
     my_AST->right_child = right_child;
 
@@ -24,7 +25,6 @@ void free_AST(struct AST *tree)
     {
         free_AST(tree->left_child);
         free_AST(tree->right_child);
-
         free_token(token);
         free(tree);
     }
--- src/AST/AST.h
+++ src/AST/AST.h
@@ -7,15 +7,23 @@
 
 struct AST
 {
-    struct token *value;
-    struct AST *left_child;
-    struct AST *right_child;
+  struct token *value;
+  enum AST_type type;
+  struct AST *left_child;
+  struct AST *right_child;
+};
+
+enum AST_type
+{
+  COMMAND,
+  ARG,
+  CONDITION, // if, elif, else clause
 };
 
 // create a tree with the right attributes and returns it.
 // returns NULL if value is NULL (wrong execution).
-struct AST *new_AST(struct token *value, struct AST *left_child,
-        struct AST *right_child);
+struct AST *new_AST(struct token *value, enum AST_type type, 
+        struct AST *left_child, struct AST *right_child);
 
 // free a every node of an AST tree
 // if tree is NULL, does nothing
