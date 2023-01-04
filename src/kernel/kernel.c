083d8cd48115795335e679bc9a08ad20e7ba46e1
Lilian Schall <lilian.schall@epita.fr>
2023-01-04 18:38:38 +0100

    feat: added kernel basic design, made interactive and script mode

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -1,13 +1,56 @@
 #include "kernel.h"
 
-int launch_shell(int options, char *file_script)
+static int launch_interactive_mode(int options);
+static int launch_script_mode(int options, char *file_script);
+
+int launch_interactive_mode(int options)
 {
-    // TODO
-    return 0;
+    char *content = NULL;
+    int status_code = 0;
+    int last_status_code = 0;
+    do
+    {
+        printf("42sh$ ");
+        fflush(stdout);
+        
+        // get content from stdin
+        content = get_interactive_content();
+
+        // execute given command
+        last_status_code = status_code;
+        status_code = execute_shell_command(options, content);
+
+        // free and dereference processed content
+        free(content);
+        content = NULL;
+    } while (status_code != -1);
+    return last_status_code;
 }
 
-int execute_shell_commands(int options, char *input)
+int launch_script_mode(int options, char *file_script)
 {
-    // TODO
-    return 0;
+    // get commands from script
+    char *input = get_file_content(file_script);
+
+    // execute input
+    return execute_shell_command(options, input);
+}
+
+int launch_shell(int options, char *file_script)
+{
+    if (!file_script)
+        return launch_interactive_mode(options);
+    return launch_script_mode(options, file_script);
+}
+
+int execute_shell_command(int options, char *input)
+{
+    // get token_list based on given input
+    struct linked_list *token_list = build_token_list(input);
+
+    // build AST based on token_list
+    struct AST *tree = build_shell_AST(token_list);
+    
+    // execute tree
+    return execute_AST(tree);
 }
--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,15 +1,21 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include "io_backend/io_backend.h"
+#include "launch_options.h"
+#include "lexer/lexer.h"
+#include "linked_list/linked_list.h"
+#include "option_parser/option_parser.h"
+#include "parser/parser.h"
+
 // launches the shell with the defined options in launch_options.h
 // if launched in interactive mode, file_script = NULL
 // returns 1 if an error occurred, else 0
 int launch_shell(int options, char *file_script);
 
-
 // function that will call each individual functions from the other modules
 // in order to execute a shell command
-// returns 1 if an error occurred, else 0
-int execute_shell_commands(int options, char *input);
+// returns the status code of the execution of the command
+int execute_shell_command(int options, char *input);
 
 #endif /* !KERNEL_H */
