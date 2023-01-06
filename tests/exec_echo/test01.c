a6bddfc184e84f5d3ffa6f4c2f417fb83fea0aff
Entinque <quentin.kuttler@epita.fr>
2023-01-06 16:30:07 +0100

    fix: test01.c

--- tests/exec_echo/test01.c
+++ tests/exec_echo/test01.c
@@ -1,50 +1,34 @@
-#include <criterion/criterion.h>
-#include <criterion/redirect.h>
-
 #include "AST/AST.h"
-#include "builtin/exec_echo.h"
+#include "builtin/builtin.h"
 #include "linked_list/linked_list.h"
 #include "token/token.h"
 
-
-void print_list(struct linked_list *ast_list)
-{
-    printf("-----");
-
-    while(ast_list->head != NULL)
-    {
-        char *s = list_head(ast_list);
-        printf("\n%s", s);
-        ast_list = list_pop(ast_list);
-    }
-    printf("\n----\n");
-
-}
-
-
-char *copy_string2(char *src)
-{
-    int l = strlen(src);
-    char *dest = malloc(sizeof(char) * (l + 1));
-    return strcpy(dest, src);
-}
+#include <stdio.h>
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
 
 int exec(void)
 {
-    struct AST *ast_word = new_AST(new_token(copy_string2("test \\n test"), (enum token_type) WORD), (enum AST_type) ARG, NULL);
+    struct AST *ast_1 = new_AST(new_token(copy_string("hello"), (enum token_type) WORD), (enum AST_type) ARG, NULL);   
+    struct AST *ast_2 = new_AST(new_token(copy_string("hello word!"), (enum token_type) WORD), (enum AST_type) ARG, NULL);
 
     struct linked_list *ll_ast = new_list();
-    ll_ast = list_append(ll_ast, ast_word);
+    ll_ast = list_append(ll_ast, ast_1);
+    ll_ast = list_append(ll_ast, ast_2);
     
-    struct AST *ast = new_AST(new_token(copy_string2("-Ene"), (enum token_type) WORD), (enum AST_type) COMMAND, ll_ast);
+    struct AST *ast = new_AST(new_token(copy_string("echo"), (enum token_type) WORD), (enum AST_type) COMMAND, ll_ast);
 
-    struct linked_list *ll_ast2 = new_list();
-    ll_ast2 = list_append(ll_ast2, ast);
+    struct linked_list *ll_word = get_linked_list_from_AST(ast);
+
+    int argc = 0;
+
+    char **argv = new_argv(ll_word, &argc);
     
-    struct AST *ast2 = new_AST(new_token(copy_string2("echo"), (enum token_type) WORD), (enum AST_type) COMMAND, ll_ast2);
 
-    echo_fn(ast2);
-    free_AST(ast2);
+    echo_fn(argc, argv);
+    free_argv(argc, argv);
+    free_list(ll_word);
+    free_AST(ast);
 
     return 0;
 }
@@ -54,11 +38,10 @@ void redirect_stdout(void)
     cr_redirect_stdout();
 }
 
-
 Test(exec_echo, exit_code, .init = redirect_stdout)
 {
     exec();
     fflush(stdout);
 
-    cr_assert_stdout_eq_str("test \n test");
+    cr_assert_stdout_eq_str("hello world !\n");
 }
