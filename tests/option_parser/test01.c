8b4bee04ad4a4aa5af7be184f612aecfd5977caf
lilian.schall <lilian.schall@epita.fr>
2023-01-07 13:43:12 +0100

    hotfix: testsuite was not compiling because of some conflicts with io_backend files and deprecated version of option_parser

--- tests/option_parser/test01.c
+++ tests/option_parser/test01.c
@@ -4,12 +4,13 @@
 
 Test(option_parser, test_given_file)
 {
-    int argc = 3;
-    char *argv[] = { "42sh", "-c", "file.sh", };
+    int argc = 2;
+    char *argv[] = { "42sh", "file.sh", };
 
     char *filename = NULL;
+    char *input = NULL;
 
-    int result = parse_given_args(argc, argv, &filename);
+    int result = parse_given_args(argc, argv, &filename, &input);
 
     cr_assert_eq(result, 1);
     cr_assert_str_eq(filename, "file.sh");
