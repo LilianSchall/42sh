0e6e7e972a60b84c8920fdfbaf8f006a067fb6d0
lilian.schall <lilian.schall@epita.fr>
2023-01-13 15:30:46 +0100

    feat: began implementation of for rule, need to do two more sub functions and it is done

---
+++ src/parser/for_rule.c
@@ -0,0 +1,96 @@
+#include "parser.h"
+
+static struct AST *create_for_tree(struct token *for_token, struct token *word,
+        struct AST *iter, struct AST *seq)
+{
+    struct AST *for_tree = new_AST(for_token, CONDITION, new_list());
+    list_append(for_tree->linked_list, new_AST(word, ARG, NULL));
+    list_append(for_tree->linked_list, iter);
+    list_append(for_tree->linked_list, seq);
+
+    return for_tree;
+}
+
+
+// this function shall parse the [';']
+// if this function is 
+static struct AST *parsing_semicolon(struct linked_list *token_list)
+{
+    // TODO
+}
+
+// this function shall parse the [ {'\n' 'in' { WORD } ';' | '\n' ]
+// if there is no word WORD, the function shall return an ITER AST with no child in it
+static struct AST *parsing_in_clause(struct linked_list *token_list)
+{
+    // TODO
+}
+
+struct AST *rule_for_rule(struct linked_list *token_list, bool trigger_warn)
+{
+    struct token *for_token = NULL;
+    struct token *word = NULL;
+    struct token *do_token = NULL;
+
+    struct log_info infos = { .trigger_warn = trigger_warn,
+        .sym = "FOR", .rulename = "rule_for_rule" };
+
+    if (!(for_token = consume_token(token_list, FOR, infos)))
+        goto for_error;
+
+    infos.sym = "WORD";
+    if (!(word = consume_token(token_list, WORD, infos)))
+    {
+        goto for_error;
+    }
+    
+    // now we have to parse [';'] | [ {'\n'} 'in' { WORD } ';' | '\n' ]
+    
+    struct AST *iter = parsing_semicolon(token_list);
+
+    if (!iter)
+    {
+        iter = parsing_in_clause(token_list);
+
+        if (!iter)
+        {
+            if (trigger_warn)
+                warnx("missing iterator on rule_for_rule");
+            goto for_error;
+        }
+    }
+
+    // now we can purge any \n
+    purge_newline_token(token_list);
+
+    // parsing do
+    info.sym = "DO";
+    if (!(do_token = consume_token(token_list, DO, infos)))
+        goto for_error;
+
+    // parsing compount_list
+    struct AST *seq = compount_list_rule(token_list, trigger_warn);
+
+    if (!seq)
+        goto free_tree;
+
+    // parsing done
+    info.sym = "DONE";
+    struct token *done_token = NULL;
+
+    if (!(done_token = consume_token(token_list, DONE, infos)))
+        goto free_tree;
+    free_token(done_token);
+
+    return create_for_tree(for_token, word, iter, seq);
+
+free_tree:
+    free_AST(iter);
+    free_AST(seq);
+
+    for_error:
+    free_token(do_token);
+    free_token(word);
+    free_token(for_token);
+    return NULL;
+}
