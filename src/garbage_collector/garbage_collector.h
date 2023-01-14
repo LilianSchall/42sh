5f70a326ac2b8f94c55444e2fc7d0c395e3d401b
lilian.schall <lilian.schall@epita.fr>
2023-01-14 10:03:32 +0100

    feat: created a garbage_collector, next step is to implement it in the project.

---
+++ src/garbage_collector/garbage_collector.h
@@ -0,0 +1,25 @@
+#ifndef GARBAGE_COLLECTOR_H
+#define GARBAGE_COLLECTOR_H
+
+#include <err.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct garbage_list
+{
+    void *ptr;
+    struct garbage_list *next;
+};
+
+static struct garbage_list *garbage_collector = NULL;
+
+void new_garbage_collector(void);
+void *mem_malloc(size_t size);
+void *mem_calloc(size_t nmemb, size_t size);
+void *mem_realloc(void *ptr, size_t size);
+
+void mem_free(void *ptr);
+
+void free_garbage_collector(void);
+
+#endif /* GARBAGE_COLLECTOR_H */
