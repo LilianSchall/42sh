9c65d12250a0679b90beebf0c9010b4d499d168c
Lilian Schall <lilian.schall@epita.fr>
2023-01-14 15:27:30 +0100

    feat: added more test to parser and lexer

--- tests/lexer/test05.c
+++ tests/lexer/test05.c
@@ -30,3 +30,25 @@ Test(lexer, echo_quote_double_quote)
 
     deep_free_list(token_list, free_token);
 }
+
+Test(lexer, until_true_do_echo_dollar_foo)
+{
+    char input[] = "until true; do echo $foo \";\" ; done";
+
+    struct linked_list *token_list = build_token_list(input);
+
+    struct linked_node *node = token_list->head;
+    
+    test_token(&node, "until", UNTIL);
+    test_token(&node, "true", WORD);
+    test_token(&node, ";", SEMICOLON);
+    test_token(&node, "do", DO);
+    test_token(&node, "echo", WORD);
+    test_token(&node, "$foo", WORD);
+    test_token(&node, ";", WORD);
+    test_token(&node, ";", SEMICOLON);
+    test_token(&node, "done", DONE);
+    cr_expect_eq(node, NULL);
+
+    deep_free_list(token_list, free_token);
+}
