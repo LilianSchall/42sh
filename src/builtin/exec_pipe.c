8a504c59d1b8782466df80788875a048e7ba41d9
Entinque <quentin.kuttler@epita.fr>
2023-01-14 15:45:06 +0100

    fix: sequence with pipe & command
    fix : example: echo foooo | cat -e; echo fooo2

--- src/builtin/exec_pipe.c
+++ src/builtin/exec_pipe.c
@@ -8,6 +8,7 @@ int execute_AST_pipe(struct AST *tree)
     int pid;
 
     int save2 = dup(STDIN_FILENO);
+    int save = dup(STDOUT_FILENO);
 
     // on récupere le premier fils
     struct linked_node *child_list = tree->linked_list->head;
@@ -48,8 +49,11 @@ int execute_AST_pipe(struct AST *tree)
     // execute la dernière commande (sera écrite dans stdout)
     int result = execute_AST(child);
 
-            dup2(save2, STDIN_FILENO);
     close(fd[0]);
     close(fd[1]);
+
+    dup2(save2, STDIN_FILENO);
+
+    dup2(save, STDOUT_FILENO); 
     return result;
 }
