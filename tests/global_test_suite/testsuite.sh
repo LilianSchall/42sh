7b00098b2dd7ca9cf970d77e35893b45a613a6b1
theo.schandel <theo.schandel@epita.fr>
2023-01-23 22:50:00 +0100

    test: new test file + optimized testsuite in bash

--- tests/global_test_suite/testsuite.sh
+++ tests/global_test_suite/testsuite.sh
@@ -1,38 +1,35 @@
 #!/bin/sh
 
 fichier=$1
-repotest=`pwd`
-j=0
-i=0
+total=0
+passed=0
 binary=$2
 while read -r line
 do
-     echo $line | $binary -c 1> 42sh_stdout  2>tmp
-     echo $line | bash --posix 1> bash_stdout 2>tmp
-     DIFF_STDOUT=`diff 42sh_stdout bash_stdout`
-     echo $line | $binary -c 2> 42sh_stderr   1> tmp
-     echo $line | bash --posix 2> bash_stderr 1> tmp
-     DIFF_STDERR=`diff 42sh_stderr bash_stderr`
-     $binary -c "$line" > tmp 2>&1
-     return_42=$?
-     bash --posix -c "$line" > tmp 2>&1
-     bash_return=$?
-     ((j++))
-     if [ "$DIFF_STDOUT" != "" ] || [ "$DIFF_STDERR" != "" ] || [[ $return_42 != $bash_return ]]
-     then
-         echo test failed: $line
-         echo -e "\n"
-         echo 42sh_stdout: `cat 42sh_stdout`
-         echo -e "bash_stdout: `cat bash_stdout` \n"
-	 echo 42sh_stderr: `cat 42sh_stderr`
-	 echo -e "bash_stderr: `cat bash_stderr` \n"
-         echo 42sh_return_value: $return_42
-         echo -e "bash_return_value: $bash_return \n__________________________\n"
-     else
-	     ((i++))
-     fi
-     rm -f 42sh_stdout bash_stdout 42sh_stderr bash_stderr
+    echo $line | $binary -c 1> .42sh_stdout  2> .42sh_stderr
+    echo $line | bash --posix 1> .bash_stdout 2> .bash_stderr
+    DIFF_STDOUT=`diff .42sh_stdout .bash_stdout`
+    DIFF_STDERR=`diff .42sh_stderr .bash_stderr`
+    $binary -c "$line" > tmp 2>&1
+    return_42=$?
+    bash --posix -c "$line" > tmp 2>&1
+    bash_return=$?
+    total=$((total + 1))
+    if [ "$DIFF_STDOUT" != "" ] || [ "$DIFF_STDERR" != "" ] || [ $return_42 != $bash_return ]
+    then
+        echo test num $total failed: "$line"
+        echo "\n"
+        echo "42sh_stdout: `cat .42sh_stdout` \n"
+        echo "bash_stdout: `cat .bash_stdout` \n"
+	    echo "42sh_stderr: `cat .42sh_stderr` \n"
+	    echo "bash_stderr: `cat .bash_stderr` \n"
+        echo 42sh_return_value: $return_42
+        echo "bash_return_value: $bash_return \n__________________________\n"
+    else
+        passed=$((passed + 1))
+    fi
+    rm -f .42sh_stdout .bash_stdout .42sh_stderr .bash_stderr
 done < "$fichier"
-echo $i/$j SUCCESSFUL TESTS
+echo $passed/$total SUCCESSFUL TESTS
 rm k.tmp
 rm tmp
